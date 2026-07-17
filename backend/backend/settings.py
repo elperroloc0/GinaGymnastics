c8a6f7898abfac64a2cd482d6e5d2efc8a4b1a47
Anjulio <potable-rarity-0b@icloud.com>
2026-07-17 01:09:29 -0400

    Add JWT authentication to REST framework and update URL patterns

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,6 +76,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -11,11 +11,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('webhooks/', include("tracking.urls")),
     path('api/', include("tracking.api_urls")),
     path('api/', include("fleet.urls")),
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
