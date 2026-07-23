a0eaf79b6d0c68b6215490180512671447b3a39e
Anjulio <potable-rarity-0b@icloud.com>
2026-07-22 20:21:21 -0400

    Enhance debug_sms task with automatic retry and backoff strategy

--- backend/notifications/tasks.py
+++ backend/notifications/tasks.py
@@ -8,7 +8,7 @@ from twilio.rest import Client
 def debug_notification_task():
     print("Task done")
 
-@shared_task
+@shared_task(autoretry_for=(Exception,), retry_backoff=True,retry_kwargs={"max_retries":5})
 def debug_sms(phone, text):
     client = Client(os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"])
 
